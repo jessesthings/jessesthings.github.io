1ac4c540517ef0973b9de629c0be141a6f5fdd95
JesseIguess <32250628+BlueberryTech123@users.noreply.github.com>
2023-10-10 00:25:25 -0700

    Added contact information

--- script.js
+++ script.js
@@ -8,4 +8,8 @@ const about = `
 
 const contact = `
 <h1>Contact</h1>
+<ul>
+    <li>Email: <a href="mailto:blueberry21007@gmail.com">blueberry21007@gmail.com</a></li>
+    <li>School Email: <a href="mailto:jl33092@pausd.us">jl33092@pausd.us</a></li>
+</ul>
 `;
